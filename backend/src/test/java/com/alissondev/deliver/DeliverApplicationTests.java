b1b2b16d230723e26569f7a13a1fa0492bd58fe2
alisson youssf <youssfbr@gmail.com>
2021-01-04 10:02:09 -0300

    Project created

---
+++ backend/src/test/java/com/alissondev/deliver/DeliverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alissondev.deliver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeliverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
