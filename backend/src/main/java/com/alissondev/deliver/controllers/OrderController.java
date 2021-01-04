e167cae27d7054edc992ef896e8c6b52eb89a7ad
alisson youssf <youssfbr@gmail.com>
2021-01-04 18:45:55 -0300

    POST /orders

--- backend/src/main/java/com/alissondev/deliver/controllers/OrderController.java
+++ backend/src/main/java/com/alissondev/deliver/controllers/OrderController.java
@@ -1,12 +1,16 @@
 package com.alissondev.deliver.controllers;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.alissondev.deliver.dto.OrderDTO;
 import com.alissondev.deliver.services.OrderService;
@@ -21,5 +25,15 @@ public class OrderController {
 	@GetMapping
 	public ResponseEntity<List<OrderDTO>> findAll() {
 		return ResponseEntity.ok(service.findAll());
+	}
+	
+	@PostMapping
+	public ResponseEntity<OrderDTO> insert(@RequestBody OrderDTO dto) {
+		dto = service.insert(dto);
+		
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(dto.getId()).toUri();
+		
+		return ResponseEntity.created(uri).body(dto);
+				
 	}
 }
